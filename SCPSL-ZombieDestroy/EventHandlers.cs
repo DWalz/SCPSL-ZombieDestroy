eff5f942a5c049b2f40ba776c98f5bbc445610bf
DWalz <d.walz@zoho.com>
2020-04-27 13:33:20 +0200

    Add Plugin, 3 Zombie attacks destroy door

---
+++ SCPSL-ZombieDestroy/EventHandlers.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EXILED;
+using EXILED.Extensions;
+using UnityEngine;
+
+namespace SCPSL_ZombieDestroy
+{
+    public class EventHandlers
+    {
+        private static Dictionary<Door, List<ReferenceHub>> attackedDoors = new Dictionary<Door, List<ReferenceHub>>();
+        
+        public void OnPlayerDoorInteract(ref DoorInteractionEvent ev)
+        {
+            if (ev.Player.GetRole() == RoleType.Scp0492)
+            {
+                Log.Info(ev.Player.GetNickname() + " is attacking the door.");
+                if (attackedDoors.TryGetValue(ev.Door, out List<ReferenceHub> players))
+                {
+                    // if (!players.Contains(ev.Player)) {
+                    players.Add(ev.Player);
+                    attackedDoors[ev.Door] = players;
+                    // }
+                }
+                else
+                {
+                    attackedDoors.Add(ev.Door, new List<ReferenceHub> {ev.Player});
+                }
+                doorAttackTimeout(ev.Door, ev.Player);
+                destroyDoorOnAttack(ev.Door);
+            }
+        }
+
+        private void destroyDoorOnAttack(Door door)
+        {
+            Log.Info(attackedDoors[door].Count + "/3");
+            if (attackedDoors[door].Count >= 3)
+            {
+                Log.Info("Door destroyed");
+                door.DestroyDoor(true);
+                door.Networkdestroyed = true;
+                attackedDoors.Remove(door);
+            }
+        }
+
+        private async void doorAttackTimeout(Door door, ReferenceHub player)
+        {
+            await Task.Delay(10000);
+            Log.Info(player.GetNickname() + " is no longer attacking the door.");
+            attackedDoors[door].Remove(player);
+        }
+        
+    }
+}
---
+++ SCPSL-ZombieDestroy/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EXILED;
+
+namespace SCPSL_ZombieDestroy
+{
+    public class ZombieDestroy : Plugin
+    {
+
+        private string _name = "ZombieDestroy";
+
+        private EventHandlers _handlers;
+        
+        public override void OnEnable()
+        {
+            Log.Info("Enabling ZombieDestroy");
+            Log.Info("Registering event handlers");
+            try
+            {
+                _handlers = new EventHandlers();
+                Events.DoorInteractEvent += _handlers.OnPlayerDoorInteract;
+            }
+            catch (Exception)
+            {
+                Log.Error("Registering event handlers failed!");
+            }
+        }
+
+        public override void OnDisable()
+        {
+            Log.Info("De-registering event handlers");
+            Events.DoorInteractEvent -= _handlers.OnPlayerDoorInteract;
+            _handlers = null;
+        }
+
+        public override void OnReload()
+        {
+            OnDisable();
+            OnEnable();
+        }
+
+        public override string getName { get => _name; }
+    }
+}
